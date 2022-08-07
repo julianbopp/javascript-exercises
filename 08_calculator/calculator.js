88700ccd3640bc15ba2387b964c890bc529d5afa
julianbopp <julian.bopp@tutanota.com>
2022-08-07 23:11:57 +0200

    complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,50 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
+};
+
+const sum = function(array) {
+  if (typeof array === "undefined") {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce(function (total, current) {
+    return total + current;
+  });
+
+
+
+};
+
+const multiply = function(array) {
+  if (typeof array === "undefined") {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce(function (total, current) {
+    return total * current;
+  });
+
+};
+
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a-1);
+  }
 	
 };
 
