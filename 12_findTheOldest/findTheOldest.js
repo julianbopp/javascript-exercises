b77b048b3d4ab6d30b25d64088101e4cdf1909dd
julianbopp <julian.bopp@tutanota.com>
2022-08-08 20:59:29 +0200

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce(function (oldest, current) {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        if (oldestAge < currentAge) {
+            return current;
+        } else {
+            return oldest;
+        }
+    })
 };
+
+const getAge = function(yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+        return new Date().getFullYear() - yearOfBirth;
+    } else {
+        return yearOfDeath - yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
