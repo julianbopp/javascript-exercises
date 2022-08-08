845cc85edf3a40ba820320fec683780040d8e7f8
julianbopp <julian.bopp@tutanota.com>
2022-08-08 16:16:07 +0200

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (variable) {
+    variable = variable.toLowerCase();
+    variable = variable.replace(/[^A-Z0-9]/ig, "");
+    variable = Array.from(variable);
 
+    const n = variable.length;
+
+    for (let i = 0; i < n/2; i++) {
+        if (variable[i] !== variable[n-i-1]) {
+            return false;
+        }
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
