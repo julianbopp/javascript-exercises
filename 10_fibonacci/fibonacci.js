0dba5a510aa44385073a5b96b33ac4c87f684b72
julianbopp <julian.bopp@tutanota.com>
2022-08-08 16:40:59 +0200

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    var next = 1;
+    var prev1 = 1;
+    var prev2 = 0;
 
+    for (let i = 0; i < n; i++) {
+        next = prev1 + prev2; 
+        prev1 = prev2;
+        prev2 = next;
+    }
+    return next;
 };
 
 // Do not edit below this line
